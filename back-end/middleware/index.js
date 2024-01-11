5476b44c0b4eb1f5cb76e8f502243f60bf1dce0d
Chris Stamper <cstampok@gmail.com>
2024-01-11 13:35:37 -0600

    clean up

--- back-end/middleware/index.js
+++
@@ -1,8 +0,0 @@
-// ./middleware/index.js
-
-import errorHandler from './errorHandler.js';
-import rateLimiter from './rateLimiter.js';
-import requireRole from './roleCheck.js';
-import { requestLogger, logger } from './requestLogger.js';
-
-export { errorHandler, rateLimiter, requireRole, requestLogger, logger };
--- back-end/services/ReviewService.js
+++
@@ -1,1 +0,0 @@
-// ./services/ReviewService.js
